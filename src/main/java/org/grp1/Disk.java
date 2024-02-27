e794cd61b07192e7ee4848fdac47e87cff356289
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2024-02-27 19:57:53 +0800

    Implement tsv parsing

--- src/main/java/org/grp1/Disk.java
+++ src/main/java/org/grp1/Disk.java
@@ -1,14 +1,55 @@
 package org.grp1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Disk {
     private final int diskSize;
     private final int blockSize;
     private final int recordSize;
+    private final ArrayList<Block> blocks;
+    private String dataFilePath;
 
-    public Disk(int diskSize, int blockSize, int recordSize) {
+    public Disk(int diskSize, int blockSize, int recordSize, String dataFilePath) {
         this.diskSize = diskSize;
         this.blockSize = blockSize;
         this.recordSize = recordSize;
+        this.blocks = getBlocksFromTSV(dataFilePath);
     }
+
+    public ArrayList<Block> getBlocksFromTSV(String dataFilePath) {
+        ArrayList<Record> records = getRecordsFromTSV(dataFilePath);
+        ArrayList<Block> blocks = new ArrayList<>();
+        return blocks;
+    }
+
+    public ArrayList<Record> getRecordsFromTSV(String dataFilePath) {
+        ArrayList<Record> records = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(dataFilePath));
+            reader.readLine();
+
+            String line;
+            while (!(line = reader.readLine()).isEmpty()) {
+                String[] recordValues = line.split("\\s+");
+                records.add(new Record(recordValues[0], Float.parseFloat(recordValues[1]), Integer.parseInt(recordValues[2])));
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error while finding file");
+        } catch (IOException e) {
+            System.out.println("Error while doing IO");
+        } catch (Exception e) {
+            System.out.println("Error while reading TSV");
+        }
+
+        return records;
+    }
+
 
 }
--- src/main/java/org/grp1/Main.java
+++ src/main/java/org/grp1/Main.java
@@ -3,14 +3,15 @@ package org.grp1;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static Disk disk;
-    public static int MB = 1 << 20;
+    private static final int MB = 1 << 20;
     private static final int DISK_SIZE = 100 * MB;
     private static final int BLOCK_SIZE = 200;
-    public static int RECORD_SIZE = 17;
+    private static final String DATA_FILE_PATH = "data.tsv";
+    private static final int RECORD_SIZE = 17;
+    private static Disk disk;
 
     public static void main(String[] args) {
-        disk = new Disk(DISK_SIZE, BLOCK_SIZE, RECORD_SIZE);
+        disk = new Disk(DISK_SIZE, BLOCK_SIZE, RECORD_SIZE, DATA_FILE_PATH);
         runExperiment1();
         runExperiment2();
         runExperiment3();
@@ -18,23 +19,27 @@ public class Main {
         runExperiment5();
     }
 
-    public static void runExperiment1 () {
+    public static void runExperiment1() {
         System.out.println("Running experiment 1");
         System.out.println("Ending experiment 1");
     }
-    public static void runExperiment2 () {
+
+    public static void runExperiment2() {
         System.out.println("Running experiment 2");
         System.out.println("Ending experiment 2");
     }
-    public static void runExperiment3 () {
+
+    public static void runExperiment3() {
         System.out.println("Running experiment 3");
         System.out.println("Ending experiment 3");
     }
-    public static void runExperiment4 () {
+
+    public static void runExperiment4() {
         System.out.println("Running experiment 4");
         System.out.println("Ending experiment 4");
     }
-    public static void runExperiment5 () {
+
+    public static void runExperiment5() {
         System.out.println("Running experiment 5");
         System.out.println("Ending experiment 5");
     }
