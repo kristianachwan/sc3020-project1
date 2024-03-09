00075197dce858e817619bc22e1b039d8a79d89e
kristianachwan <kristianachwan@gmail.com>
2024-03-09 20:45:01 +0800

    Add more config

--- src/main/java/org/grp1/constant/Config.java
+++ src/main/java/org/grp1/constant/Config.java
@@ -1,12 +1,15 @@
 package org.grp1.constant;
 
 public class Config {
-    
+
     public static final int MB = 1 << 20;
     public static final int DISK_SIZE = 100 * MB;
     public static final int BLOCK_SIZE = 200;
     public static final String DATA_FILE_PATH = "data.tsv";
     public static final int RECORD_SIZE = 17;
+    public static final int NUMBER_OF_RECORDS_IN_BLOCK = BLOCK_SIZE / RECORD_SIZE;
+    public static final int POINTER_SIZE = 8;
+    public static final int KEY_SIZE = 4;
+    public static final int N = (BLOCK_SIZE - POINTER_SIZE) / (KEY_SIZE + POINTER_SIZE);
     public static final int TREE_NODE_SIZE = 13;
-
 }
